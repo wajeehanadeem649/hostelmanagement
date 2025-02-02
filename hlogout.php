97ab918f119d5b4dbc99945425e2f20d31129f37
Wajeeha Nadeem <150377263+wajeehanadeem649@users.noreply.github.com>
2025-02-02 23:19:29 +0500

    Add files via upload

---
+++ hlogout.php
@@ -0,0 +1,37 @@
+
+<?php
+
+session_start();
+session_unset();
+session_destroy();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+        <!--bootstrap css-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- fontawesome-->
+    <link rel="stylesheet" href="fontjs/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+
+     <!--google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<!--custom css-->
+</head>
+<body>
+   
+<h3>Successfully Logout</h3>
+
+<script src="js/bootstrap.min.js"></script>
+<scrip src="fontjs/all.min.js"></script>
+</body>
+</html>
